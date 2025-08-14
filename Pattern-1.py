9f5f202e39b337b3973742657543ed9390150d72
Dharshika Pugalenthi <dharshikapugal@gmail.com>
2025-08-14 20:38:22 +0530

    pattern-1 added

---
+++ Pattern-1.py
@@ -0,0 +1,18 @@
+n=int(input("Enter the number of rows needed: "))
+for i in range(n):
+    for j in range(i+1):
+        print(j+1, end=' ')
+    print()
+
+""" 1 
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5 """
+
+""" For hashing multiple lines, use alt+shift+A
+eshadhhhhbdksjhbakjhbue
+awjhbuewgfsnbmnbz
+hbwedshga """
+
+# To hash as single line, use ctrl+/
